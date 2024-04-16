d40f473303eee81456be823f8d8941865c18abdc
Anwesh Gangula <gangulaanwesh@gmail.com>
2024-04-16 21:33:14 +0530

    display page name first without heirarchy

--- src/components/LogseqPage.tsx
+++ src/components/LogseqPage.tsx
@@ -13,18 +13,27 @@ const LogseqPageLink = ({
   graph,
 }: LogseqPageLinkProps) => {
 
-  if (page === undefined || page?.name === undefined) {
+  if (page === undefined || page?.name === undefined || page?.originalName === undefined) {
     return <></>;
   }
+
+  const pagePath = page.originalName.split("/")
+  const pageName = pagePath.splice(-1)[0].trim(); // note splice remove item from original array
 
   return (
     <>
       <a
         className={styles.logseqPageLink}
         href={`logseq://graph/${graph}?page=${page?.name}`}
+        title={page?.originalName}
       >
         <span className="tie tie-page"></span>
-        {page?.originalName?.replaceAll("/", "/ ")}
+        <span>{pageName}</span>
+        {pagePath.length > 0 && (
+          <small>
+            {` (${pagePath.join("/")})`}
+          </small>
+        )}
       </a>
     </>
   );
